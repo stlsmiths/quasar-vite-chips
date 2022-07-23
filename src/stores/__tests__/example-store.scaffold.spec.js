f617240459fe57c216db69c6c18ba18f71bd9ad0
todd <todd@stlsmiths.com>
2022-07-23 15:17:17 -0500

    start testing stuff

---
+++ src/stores/__tests__/example-store.scaffold.spec.js
@@ -0,0 +1,16 @@
+import {describe,it} from 'vitest'
+
+describe('example store tests', () => {
+
+  it('store exists', () => {})
+
+  describe('state', () => {
+    it('exists', () => {})
+    it('has proper initialState', () => {})
+  })
+  describe('getters', () => {
+    it('work and return as expected', () => {})
+
+  })
+
+})
